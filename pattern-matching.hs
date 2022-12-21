1e64383bb9c8fcf258c9b0d580a6d77d57e98bd8
Haiaty Varotto <varottohaiaty@gmail.com>
2022-12-21 12:17:27 +0100

    Update pattern-matching.hs

--- pattern-matching.hs
+++ pattern-matching.hs
@@ -51,11 +51,39 @@ module Example where
   
   -- implementation of lenght using recursion and pattern matching
   
+  -- First we defined the result of a known input — the empty list. This is also known as the edge condition
+  -- Then in the second pattern we take the list apart by splitting it into a head and a tail. We say that the length is equal to 1 plus the length of the tail. We use _ to match the head because we don't actually care what it is. Also note that we've taken care of all possible patterns of a list.
+  -- The first pattern matches an empty list and the second one matches anything that isn't an empty list.
+  
   -- (_:xs) means: _ discard the first element, xs is the rest of the list
   -- so it will be 1 + (1 + (1 + ... (1 + 0)))
   length' :: (Num b) => [a] -> b
   length' [] = 0
   length' (_:xs) = 1 + length' xs 
+  
+  
+  -- implmentation of sum of elements of a list using recursion and pattern matching
+  
+  -- We know that the sum of an empty list is 0. We write that down as a pattern. 
+  -- And we also know that the sum of a list is the head plus the sum of the rest of the list
+  sum' :: (Num a) => [a] -> a  
+  sum' [] = 0  
+  sum' (x:xs) = x + sum' xs
+  
+  
+  ------------------------
+  -- 'NAMED' PATTERNS
+  ------------------------
+  -- as patterns. Those are a handy way of breaking something up according to a pattern and binding it to names whilst still keeping a reference to the whole thing. 
+  -- You do that by putting a name and an @ in front of a pattern.
+  
+  -- Normally we use as patterns to avoid repeating ourselves when matching against a bigger pattern when we have to use the whole thing again in the function body
+  
+  -- example:
+  -- in this case the pattern is called 'all' and is used in the defintion
+  capital :: String -> String  
+  capital "" = "Empty string, whoops!"  
+  capital all@(x:xs) = "The first letter of " ++ all ++ " is " ++ [x]
   
   
   

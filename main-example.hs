1bd4d7ee216f894dd0f1520c3799e0a4f110310e
Haiaty Varotto <varottohaiaty@gmail.com>
2022-12-21 12:45:46 +0100

    Update main-example.hs

--- main-example.hs
+++ main-example.hs
@@ -1,22 +1,53 @@
 -- you can import other modules using 'import'
 
-
+----------------
 -- example 1.hs
+-----------------
 main = putStrLn "Hello from Haskell"
 
-
+---------------------
 -- file example 2.hs
+---------------------
 main = main = do
     print("hello")
     
-    
+ ---------------------
  -- file example3.hs
+ ----------------------
  -- with declaration
  
  main :: IO()
  main = putStrLn "Hello from Haskell"
  
  
+ 
+ ---------------------------
+ -- file example 4.hs
+ -------------------------
+ -- with variable, and declaration
+ -- NOTE: the variable could be put after the main. it is not necessary to come before the mai definition
+ 
+greeting = "His"
+
+main :: IO ()
+main = do
+    print(greeting ++ " world")
+    
+    
+    
+ ---------------------   
+ -- file example 5.hs
+ -----------------------
+ -- with variable, and declaration, and a function
+ 
+greeting = "His"
+
+main :: IO ()
+main = do
+    print(greeting ++ " world")
+    print(greet "io")
+ 
+greet who = greeting ++ " " + who
  
  ---------------
  -- HOW TO RUN
